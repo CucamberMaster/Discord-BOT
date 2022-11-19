d77df1bf6c1d8a0934acb6aaa6810817959e68b6
Kacper Ogorkiewicz <kacperogorkiewicz777@gmail.com>
2022-11-19 21:15:03 +0100

    Bot DISCORD

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,7 +2,7 @@ const client = require('..');
 const {EmbedBuilder} = require('discord.js');
 
 client.on('guildMemberAdd', async member => {
-    // member.guild.channels.cache.get('1030484724700950631').send(`${member.user} has joined the server `);
+    // member.guild.channels.cache.get('yourchannelid').send(`${member.user} has joined the server `);
 
     const newMemberEmbed = new EmbedBuilder()
         .setColor(0x7289DA)
@@ -10,7 +10,7 @@ client.on('guildMemberAdd', async member => {
         .setDescription(` User ${member.user} has join the server`)
         .setThumbnail(member.user.displayAvatarURL())
         .setTimestamp();
-    member.guild.channels.cache.get('1030484724700950631').send(
+    member.guild.channels.cache.get('yourchannelid').send(
         {
             embeds: [newMemberEmbed]
         }
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,6 +1,6 @@
 const client = require('..');
 
 client.on('guildMemberRemove', async member => {
-    member.guild.channels.cache.get('1030484724700950631')
+    member.guild.channels.cache.get('yourchannelid')
         .send(`${member.user.username} has left the server, We're sorry to heard that :/ `);
 });
